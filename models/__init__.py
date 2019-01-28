75a06ea6d562f13f952e249667c4f33e65677341
zengxianyu <zengyu@mail.dlut.edu.cn>
2019-01-28 14:56:17 +0800

    rename

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,4 @@ from .sal_model import SalModel
 from .depth_model import DepthModel
 from .seg_model import SegModel
 # from .det_model import DetModel
-from .gan_models import GANModel
+from .gan_model import GANModel
